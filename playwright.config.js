f6f14abdcd529a6d0ae0dbd3ae3d997362929619
Hernani Fernandes <hfernandes+rituals@xebia.com>
2026-01-22 10:06:03 +0100

    Update Playwright config to support testing built app

--- playwright.config.js
+++ playwright.config.js
@@ -20,7 +20,7 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
   ],
-  webServer: {
+  webServer: process.env.TEST_BUILT_APP ? undefined : {
     command: 'npm run dev',
     url: 'http://localhost:5173',
     reuseExistingServer: !process.env.CI,
